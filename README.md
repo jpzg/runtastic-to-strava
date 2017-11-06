ef4d4c3d343a512e988ec5b0215910cd0f87a448
jpzg <jacobgrossbard@gmail.com>
2017-11-05 23:47:41 -0800

    Lists summary of Strava and Runtastic activities

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Automatically move your Runtastic activities to Strava.
 
 This is a tool for Windows Phone users who like strava, enabling them to use the (very nice) Runtastic app for Windows Phone while still getting all of their activities onto strava hassle-free.
+To use this, you must have an access token for your account. Get one by creating an application at https://strava.com/settings/api.
--- main.go
+++ main.go
@@ -1,19 +1,24 @@
 package main
 
 import (
-	"os"
-	"fmt"
+	"context"
 	"flag"
-	
+	"fmt"
+	"os"
+
+	"github.com/golang/glog"
+	"github.com/metalnem/runtastic/api"
 	"github.com/pkg/errors"
-	//"github.com/metalnem/runtastic/api"
+	"github.com/strava/go.strava"
 )
 
 var (
-	email	 = flag.String("email","","")
-	password = flag.String("password","","")
-	
-	errMissingCredentials = errors.New("Missing email address or password")
+	email    = flag.String("email", "", "Email address for your Runtastic account. May be stored in the environment variable RUNTASTIC_EMAIL.")
+	password = flag.String("password", "", "Password for your Runtastic account. May be stored in the environment variable RUNTASTIC_PASSWORD.")
+	token    = flag.String("token", "", "The Strava access token for your account. See strava.com/settings/api. May be stored in the environment variable STRAVA_TOKEN.")
+	thorough = flag.Bool("thorough", false, "Check that all Runtastic activities are on Strava (not just all activities until the last recorded Strava activity)")
+
+	errMissingCredentials = errors.New("Missing email address, password, or access token")
 )
 
 const usage = `Usage of Runtastic->Strava Importer:
@@ -21,37 +26,80 @@ const usage = `Usage of Runtastic->Strava Importer:
 		Email (required)
 		
   -password string
-		Password(required)`
+		Password(required)
 		
-func getCredentials() (string, string, error) {
+  -token string
+		Strava access token for your account (required)
+		
+  -thorough
+		Checks that all activities on the Runtastic account are also on Strava. Normal check goes only until the last recorded Strava activity`
+
+func getInput() (string, string, string, bool, error) {
 	email := *email
 	password := *password
-	
-	if email != "" && password != "" {
-		return email, password, nil
+	token := *token
+	thorough := *thorough
+
+	if email != "" && password != "" && token != "" {
+		return email, password, token, thorough, nil
 	}
-	
+
 	email = os.Getenv("RUNTASTIC_EMAIL")
 	password = os.Getenv("RUNTASTIC_PASSWORD")
-	
-	if email != "" && password != "" {
-		return email, password, nil
+	token = os.Getenv("STRAVA_TOKEN")
+
+	if email != "" && password != "" && token != "" {
+		return email, password, token, thorough, nil
 	}
-	
-	return "", "", errMissingCredentials
+
+	return "", "", "", false, errMissingCredentials
 }
 
 func main() {
 	flag.Parse()
-	
-	email, password, err := getCredentials()
-	
+
+	email, password, token, thorough, err := getInput()
+
 	if err != nil {
 		fmt.Println(usage)
 		os.Exit(1)
 	}
+
+	if thorough {
+		fmt.Println("Thorough mode is on")
+	}
 	
-	fmt.Println(email)
-	fmt.Println(password)
+	ctx := context.Background()
+	session, err := api.Login(ctx, email, password)
+
+	if err != nil {
+		glog.Exit(err)
+	}
+
+	fmt.Println("Successfully logged into Runtastic")
+
+	client := strava.NewClient(token)
+	athlete := strava.NewCurrentAthleteService(client)
+	strava_meta, err := athlete.ListActivities().Page(1).Do()
+
+	if err != nil {
+		glog.Exit(err)
+	}
+
+	runtastic_meta, err := session.GetMetadata(ctx)
+
+	if err != nil {
+		glog.Exit(err)
+	}
+
+	for _, activity := range runtastic_meta {
+		fmt.Println(activity)
+	}
 	
+	for _, activity := range strava_meta {
+		fmt.Println(activity)
+	}
+
+	glog.Flush()
+
 }
